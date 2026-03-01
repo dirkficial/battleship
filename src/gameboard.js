a1c8869a9ae3efa7df7d521952c8b09b0940f0a9
Derek Kim <derek.kim175@gmail.com>
2026-03-01 09:53:41 +0900

    Implemented placeShip() method

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,10 +1,47 @@
+import Ship from "./ship.js";
+
 class Gameboard {
     constructor() {
-        const board = [];
+        this.board = [];
         for (let i = 0; i < 100; i++) {
-            board.push({hasShip: false, isHit: false});
+            this.board.push({hasShip: false, isHit: false});
         }
     }
 
-    
+    placeShip(size, coordinate, isVertical) {
+        const indicies = []
+        if (isVertical) {
+            for (let i = coordinate; i < coordinate + size*10; i += 10) {
+                indicies.push(i);
+            }
+        } 
+        if (!isVertical) {
+            if ((coordinate % 10) + size < 10) {
+                    return false;
+            }
+            for (let i = coordinate; i < coordinate + size; i++) {
+                indicies.push(i);
+            }
+        }
+
+        for (const idx of indicies) {
+            if (idx > 99 || idx < 0) {
+                return false;
+            }
+
+            if (this.board[idx].hasShip === true) {
+                return false;
+            }
+        };
+        
+        const ship = new Ship(size);
+        indicies.forEach(idx => {
+            this.board[idx].hasShip = true;
+            this.board[idx].ship = ship;
+        })
+    }
+
+    receiveAttack() {
+
+    }
 }
